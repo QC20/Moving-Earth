d97ff3126a45561713e0147cf29bf603feabc36c
ionas <36644388+QC20@users.noreply.github.com>
2025-04-27 23:15:52 +0200

    update code

--- sketch.js
+++ sketch.js
@@ -1,15 +1,11 @@
 let n = 75;
 let azimuthOffset = 0;
-
-
-let diameter, r, centreX, centreY, centre, radius;
-
+let diameter, r, centreX, centreY, radius;
 
 function setup() {
   createCanvas(800, 800);
   colorMode(HSB, 1);
   noStroke();
-  //frameRate(10);
   
   diameter = width / n;
   r = diameter / 2;
@@ -19,7 +15,6 @@ function setup() {
 }
 
 function draw() {
-
   azimuthOffset += map(mouseX, 0, width, -1, 1) * 0.05;
   background(0);
   
@@ -41,33 +36,30 @@ function draw() {
       let ns1 = noise(nx + 10, ny + 10, nz + 10);
       let ns2 = noise(nx / 2 + 10, ny / 2 + 10, nz / 2 + 10);
       let ns3 = noise(nx * 2 + 10, ny * 2 + 10, nz * 2 + 10);
-			
-			if(ns1 > 0.5){
-				ns1 = 1;
-			}else{
-				ns1 = 0;
-			}
-			
-			if(ns2 > 0.5){
-				ns2 = 1;
-			}else{
-				ns2 = 0;
-			}
-			
-			if(ns3 > 0.5){
-				ns3 = 1;
-			}else{
-				ns3 = 0;
-			}
-			
-			let ns = (ns1 + ns2 + ns3) / 3;
-			ns = pow(ns, map(mouseY, 0, height, 5, 0));
+      
+      if(ns1 > 0.5){
+        ns1 = 1;
+      }else{
+        ns1 = 0;
+      }
+      
+      if(ns2 > 0.5){
+        ns2 = 1;
+      }else{
+        ns2 = 0;
+      }
+      
+      if(ns3 > 0.5){
+        ns3 = 1;
+      }else{
+        ns3 = 0;
+      }
+      
+      let ns = (ns1 + ns2 + ns3) / 3;
+      ns = pow(ns, map(mouseY, 0, height, 5, 0));
 
-			fill(0, 0,1- ns);
-			
-      
+      fill(0, 0, 1 - ns);
       circle(x + centreX, y + centreY, diameter);
-      
     }
   }
 }
